0062de2c2f6b2f833248dca71a5035536c3a2077
Dennis Sauve <dengsauve@yahoo.com>
2019-07-22 20:38:52 -0700

    Updating Info, ignoring snippets.json

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <?php
-    
-    $contents = file_get_contents('snippets.json');
+
+    if (isset($_POST['submit'])){
+
+        // Get Variable Input
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $language = $_POST['language'];
+        $body = $_POST['body'];
+
+        $j_str = file_get_contents('resources/data/snippets.json');// Getting JSON file
+        $json = json_decode($j_str, true);// Decode
+
+        // Create array from input
+        $snippetObj = array(
+            'name' => $name,
+            'description' => $description,
+            'language' => $language,
+            'body' => $body
+        );
+
+        array_push($json['snippets'], $snippetObj); // push changes into JSON array
+
+        $j_out = json_encode($json); // Encode as JSON
+
+        // Write the changes to file
+        $fout = fopen('resources/data/snippets.json', 'w');
+        fwrite($fout, $j_out);
+        fclose($fout);
+    }
+
+    $contents = file_get_contents('resources/data/snippets.json');
     $snippets = json_decode($contents)->snippets;
     
     // Get languages and groups
@@ -24,26 +53,42 @@
         <link rel="stylesheet" href="/resources/css/snippets-style.css" />
     </head>
     <body>
+        
+        <div class="nav">
+            Navigation
+            
+            <ul>
+                <?php
+                    foreach($groups as $name => $snippets) {
+                        echo "<li><a href='#$name'>$name</a></li>";
+                    }
+                    ?>
+            </ul>
+        </div>
+        
         <h1>
             ##########################<br/>
             # Dennis's Code Snippets #<br/>
             ##########################
         </h1>
 
-        Navigation
+        <br/>
         
-        <ul>
-            <?php
-                foreach($groups as $name => $snippets) {
-                    echo "<li><a href='#$name'>$name</a></li>";
-                }
-            ?>
-        </ul>
+        <p>New Snippet</p>
+
+        <form action="/index.php" method="POST">
+            <input class="form-control" type="text" name="name" placeholder="Enter Name">
+            <input class="form-control" type="text" name="description" placeholder="Describe the snippet's function, etc.">
+            <input class="form-control" type="text" name="language" placeholder="Provide the language">
+            <textarea class="form-control" name="body" rows="8" placeholder="Code"></textarea>
+            <input type="submit" value="Add Snippet" name="submit" />
+        </form>
+
 
         <?php
         foreach($groups as $name => $snippets) {
             $top_bottom = str_repeat("#", strlen($name));
-            echo "<h2>$top_bottom<br/><a name='$name'>$name</a><br/>$top_bottom</h2>";
+            echo "<h2>$top_bottom<br/>$name<br/>$top_bottom</h2><a name='$name' class='target'></a>";
             foreach($snippets as $snippet) {
                 echo "<h3>$snippet->name <br/> <small>$snippet->description</small></h3>";
                 $body = $snippet->body;
